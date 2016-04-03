de873f7c645dd0c4d071b2daff475c748b1a88d4
Michelle Chan <michelchan421@gmail.com>
2016-04-02 23:33:45 -0400

    Added a few muse methods that came with the sample Android

--- mobile/src/main/java/com/example/story_teller/ishimuse/MainActivity.java
+++ mobile/src/main/java/com/example/story_teller/ishimuse/MainActivity.java
@@ -1,11 +1,151 @@
 package com.example.story_teller.ishimuse;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.interaxon.libmuse.Accelerometer;
+import com.interaxon.libmuse.AnnotationData;
+import com.interaxon.libmuse.ConnectionState;
+import com.interaxon.libmuse.Eeg;
+import com.interaxon.libmuse.LibMuseVersion;
+import com.interaxon.libmuse.MessageType;
+import com.interaxon.libmuse.Muse;
+import com.interaxon.libmuse.MuseArtifactPacket;
+import com.interaxon.libmuse.MuseConfiguration;
+import com.interaxon.libmuse.MuseConnectionListener;
+import com.interaxon.libmuse.MuseConnectionPacket;
+import com.interaxon.libmuse.MuseDataListener;
+import com.interaxon.libmuse.MuseDataPacket;
+import com.interaxon.libmuse.MuseDataPacketType;
+import com.interaxon.libmuse.MuseFileFactory;
+import com.interaxon.libmuse.MuseFileReader;
+import com.interaxon.libmuse.MuseFileWriter;
+import com.interaxon.libmuse.MuseManager;
+import com.interaxon.libmuse.MusePreset;
+import com.interaxon.libmuse.MuseVersion;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Muse muse = null;
+    private ConnectionListener connectionListener = null;
+    private DataListener dataListener = null;
+    private boolean dataTransmission = true;
+
+    public MainActivity() {
+        // Create listeners and pass reference to activity to them
+        WeakReference<Activity> weakActivity =
+                new WeakReference<Activity>(this);
+
+        connectionListener = new ConnectionListener(weakActivity);
+        dataListener = new DataListener(weakActivity);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    /**
+     * Connection listener updates UI with new connection status and logs it.
+     */
+    class ConnectionListener extends MuseConnectionListener {
+
+        final WeakReference<Activity> activityRef;
+
+        ConnectionListener(final WeakReference<Activity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void receiveMuseConnectionPacket(MuseConnectionPacket p) {
+            final ConnectionState current = p.getCurrentConnectionState();
+            final String status = p.getPreviousConnectionState().toString() +
+                    " -> " + current;
+            final String full = "Muse " + p.getSource().getMacAddress() +
+                    " " + status;
+            Log.i("Muse Headband", full);
+            Activity activity = activityRef.get();
+            // UI thread is used here only because we need to update
+            // TextView values. You don't have to use another thread, unless
+            // you want to run disconnect() or connect() from connection packet
+            // handler. In this case creating another thread is required.
+            if (activity != null) {
+
+            }
+        }
+    }
+
+    /**
+     * Data listener will be registered to listen for: Accelerometer,
+     * Eeg and Relative Alpha bandpower packets. In all cases we will
+     * update UI with new values.
+     * We also will log message if Artifact packets contains "blink" flag.
+     * DataListener methods will be called from execution thread. If you are
+     * implementing "serious" processing algorithms inside those listeners,
+     * consider to create another thread.
+     */
+    class DataListener extends MuseDataListener {
+
+        final WeakReference<Activity> activityRef;
+
+        DataListener(final WeakReference<Activity> activityRef) {
+            this.activityRef = activityRef;
+        }
+
+        @Override
+        public void receiveMuseDataPacket(MuseDataPacket p) {
+            switch (p.getPacketType()) {
+                case EEG:
+                    updateEeg(p.getValues());
+                    break;
+                case ACCELEROMETER:
+                    updateAccelerometer(p.getValues());
+                    break;
+                case ALPHA_RELATIVE:
+                    updateAlphaRelative(p.getValues());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void receiveMuseArtifactPacket(MuseArtifactPacket p) {
+            if (p.getHeadbandOn() && p.getBlink()) {
+                Log.i("Artifacts", "blink");
+            }
+        }
+
+        private void updateAccelerometer(final ArrayList<Double> data) {
+            Activity activity = activityRef.get();
+            if (activity != null) {
+
+            }
+        }
+
+        private void updateEeg(final ArrayList<Double> data) {
+            Activity activity = activityRef.get();
+            if (activity != null) {
+
+            }
+        }
+
+        private void updateAlphaRelative(final ArrayList<Double> data) {
+            Activity activity = activityRef.get();
+            if (activity != null) {
+
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,4 +174,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
